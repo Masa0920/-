b5150a1fbbe7ef39775d2bf632f857d5de1c10ba
Masa0920 <60780550+Masa0920@users.noreply.github.com>
2021-03-09 18:16:10 +0900

    非公開にした

--- src/screens/TopContainer.js
+++ src/screens/TopContainer.js
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom'
 
 export const TopContainer = () => {
    return (
-      <div className="topcontainer">
-         <Link to='/OMath1'><SubjectButton subject="応用数学１" /></Link>
-         {/* <Link to="/" ><SubjectButton subject="応用数学演習１" /></Link> */}
-         <Link to="/OMath2" ><SubjectButton subject="応用数学２" /></Link>
-         {/* <Link to="/" ><SubjectButton subject="応用数学演習２" /></Link> */}
-         {/* <Link to="/" ><SubjectButton subject="計算機プログラミング" /></Link> */}
-         <Link to="/ECircle" ><SubjectButton subject="電気回路" /></Link>
-         <Link to="/Info" ><SubjectButton subject="情報理論" /></Link>
-         <Link to="/Electronic" ><SubjectButton subject="電子デバイス" /></Link>
-      </div>
+      <div></div>
+      // <div className="topcontainer">
+      //    <Link to='/OMath1'><SubjectButton subject="応用数学１" /></Link>
+      //    {/* <Link to="/" ><SubjectButton subject="応用数学演習１" /></Link> */}
+      //    <Link to="/OMath2" ><SubjectButton subject="応用数学２" /></Link>
+      //    {/* <Link to="/" ><SubjectButton subject="応用数学演習２" /></Link> */}
+      //    {/* <Link to="/" ><SubjectButton subject="計算機プログラミング" /></Link> */}
+      //    <Link to="/ECircle" ><SubjectButton subject="電気回路" /></Link>
+      //    <Link to="/Info" ><SubjectButton subject="情報理論" /></Link>
+      //    <Link to="/Electronic" ><SubjectButton subject="電子デバイス" /></Link>
+      // </div>
    )
 }
 
